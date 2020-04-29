c70257b6528b22bc4f5ec67ceb62433909e32ca9
Royce Blackbourn <royce.blackbourn@my.jcu.edu.au>
2020-04-29 16:10:06 +1000

    -Switch theme to use Sass and include gulp helper files

--- themes/group20/footer.php
+++ themes/group20/footer.php
@@ -11,8 +11,6 @@
 
 ?>
 
-	</div><!-- #content -->
-
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'group20' ) ); ?>">
@@ -24,7 +22,7 @@
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'group20' ), 'group20', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'group20' ), 'group20', '<a href="http://underscores.me/">Group20</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
