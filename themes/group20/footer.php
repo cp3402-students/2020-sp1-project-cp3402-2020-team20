9eda38f0de90cecbdd5e39313b15b30bde45c6fa
Ted Houldsworth <edward.houldsworth@my.jcu.edu.au>
2020-05-18 14:14:41 +1000

    Add 3 images to the footer for location pics, uses flexbox to layout pics better on larger screens

--- themes/group20/footer.php
+++ themes/group20/footer.php
@@ -5,7 +5,7 @@
  * Contains the closing of the #content div and all content after.
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
+ * 
  * @package Group20
  */
 
@@ -13,17 +13,19 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'group20' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'group20' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
+
+			<?php wp_nav_menu( array( 'theme_location' => 'new-menu' ) ); ?>
+			
+			<div class="footer_images_row">
+				<?php echo_theme_footer_images() ?>
+			</div>
+			
+			<span class="sep"> </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'group20' ), 'group20', '<a href="http://underscores.me/">Group20</a>' );
 				?>
+
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
